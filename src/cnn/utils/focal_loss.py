c593a33fd0aa8d43deca1c41815464793818ca24
Mark Huang (R1B7 <kchuang1015@gmail.com>
2019-10-26 15:44:17 +0000

    Fix bugs of focal loss.

--- src/cnn/utils/focal_loss.py
+++ src/cnn/utils/focal_loss.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+    def __init__(self, alpha=1, gamma=2, logits=True, reduce=True):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
