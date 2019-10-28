64b6d3e9e5d1755323b00c50f6b22ac5a25f8270
Mark Huang (MSI_DT) <kchuang1015@gmail.com>
2019-10-28 14:06:43 +0800

    Add scripts

---
+++ bin/train_se_resnext50_32_4d_WP6_WD_0.1.sh
@@ -0,0 +1,6 @@
+model=model_se_resnext50_32_4d_WP6_WD_0.1
+gpu=4
+fold=0
+conf=./conf/${model}.py
+
+python -m src.cnn.main train ${conf} --fold ${fold} --gpu ${gpu}
---
+++ bin/train_se_resnext50_32_4d_WP8_WD_0.1.sh
@@ -0,0 +1,6 @@
+model=model_se_resnext50_32_4d_WP8_WD_0.1
+gpu=6
+fold=0
+conf=./conf/${model}.py
+
+python -m src.cnn.main train ${conf} --fold ${fold} --gpu ${gpu}
